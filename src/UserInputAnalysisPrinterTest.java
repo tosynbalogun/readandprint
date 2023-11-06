530b974e382002e587f62ba8a4d0834be106a479
Tosin Balogun <tosinbalogun1@gmail.com>
2023-11-06 19:27:01 +0100

    add tests

--- src/UserInputAnalysisPrinterTest.java
+++ src/UserInputAnalysisPrinterTest.java
@@ -20,4 +20,19 @@ public class UserInputAnalysisPrinterTest {
         assertEquals(printer.getRowCount(), 3);
         assertEquals(printer.getCharacterCount(), 10);
     }
+
+    @Test
+    public void testPrintUserInputAnalysisInALoopRunsAndTerminatesWithNoUserInput() {
+        UserInputAnalysisPrinter printer = new UserInputAnalysisPrinter();
+
+        // Mock the Scanner class to return the user input
+        Scanner sc = new Scanner("STOP");
+        printer.setScanner(sc);
+
+        // Call the readAndPrintUserInputInALoop() method
+        printer.printUserInputAnalysisInALoop();
+
+        assertEquals(printer.getRowCount(), 1);
+        assertEquals(printer.getCharacterCount(), 4);
+    }
 }
